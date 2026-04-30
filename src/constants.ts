f0d52178a31959bb2387959a65577e554da80f12
konstantin <gktin@yandex.ru>
2026-04-30 22:55:04 +0700

    change store

--- src/constants.ts
+++ src/constants.ts
@@ -1,95 +1,46 @@
 import type { ItemsStore } from '@/types';
 
 export const Store: ItemsStore = {
-	'1': {
-		position: { x: 400, y: 100 },
+	'14': {
+		position: { x: 200, y: 200 },
 		data: {
-			label: 'Timothée Chalamet',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Timoth%C3%A9e_Chalamet-63482_%28cropped%29.jpg/330px-Timoth%C3%A9e_Chalamet-63482_%28cropped%29.jpg',
+			label: 'Обри Плаза',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Aubrey_Plaza_at_the_2024_Cannes_Film_Festival_%28head_shot%29.jpg/330px-Aubrey_Plaza_at_the_2024_Cannes_Film_Festival_%28head_shot%29.jpg',
 		},
 	},
-	'2': {
-		position: { x: 433, y: 200 },
+	'15': {
+		position: { x: 300, y: 150 },
 		data: {
-			label: 'Milo Ventimiglia',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Milo_Ventimiglia_2019_by_Glenn_Francis.jpg/330px-Milo_Ventimiglia_2019_by_Glenn_Francis.jpg',
+			label: 'Аня Тейлор-Джой',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Anya_Taylor-Joy_at_the_2025_Toronto_International_Film_Festival._06_%28cropped%29.jpg/330px-Anya_Taylor-Joy_at_the_2025_Toronto_International_Film_Festival._06_%28cropped%29.jpg',
 		},
 	},
-	'3': {
-		position: { x: 466.66666666666663, y: 300 },
+	'16': {
+		position: { x: 150, y: 550 },
 		data: {
-			label: 'Henry Cavill',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Henry_Cavill_%2848417913146%29_%28cropped%29.jpg/330px-Henry_Cavill_%2848417913146%29_%28cropped%29.jpg',
+			label: 'Ана де Армас',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Ana_de_Armas_%2854462619561%29_%28cropped_3%29.jpg/330px-Ana_de_Armas_%2854462619561%29_%28cropped_3%29.jpg',
 		},
 	},
-	'4': {
-		position: { x: 500, y: 400 },
+	'17': {
+		position: { x: 350, y: 550 },
 		data: {
-			label: 'Ben Affleck',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Ben_Affleck_on_the_Red_Carpet%2C_SXSW_2023_%28cropped%29.jpg/330px-Ben_Affleck_on_the_Red_Carpet%2C_SXSW_2023_%28cropped%29.jpg',
+			label: 'Дайан Крюгер',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/MKr378435_Diane_Kruger_%28Amrum%2C_Cannes_2025%29.jpg/330px-MKr378435_Diane_Kruger_%28Amrum%2C_Cannes_2025%29.jpg',
 		},
 	},
-	'5': {
-		position: { x: 533, y: 100 },
+	'18': {
+		position: { x: 550, y: 550 },
 		data: {
-			label: 'Юрий Дудь',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Yury_Dud_in_Nizhnekamsk.jpg/330px-Yury_Dud_in_Nizhnekamsk.jpg',
+			label: 'Дженна Ортега',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Jenna_Ortega-63799_%28cropped%29.jpg/330px-Jenna_Ortega-63799_%28cropped%29.jpg',
 		},
 	},
-	'6': {
-		position: { x: 566, y: 200 },
+	'19': {
+		position: { x: 250, y: 650 },
 		data: {
-			label: 'Robert Pattinson',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Robert_Pattinson_at_Berlinale_2025.jpg/330px-Robert_Pattinson_at_Berlinale_2025.jpg',
-		},
-	},
-	'7': {
-		position: { x: 600, y: 300 },
-		data: {
-			label: "Josh O'Connor",
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/JoshO%27ConnorMastermindBFILFF131025-15_%2854863445326%29.jpg/330px-JoshO%27ConnorMastermindBFILFF131025-15_%2854863445326%29.jpg',
-		},
-	},
-	'8': {
-		position: { x: 633, y: 400 },
-		data: {
-			label: 'Aaron Taylor-Johnson',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Aaron_Taylor-Johnson_at_82nd_Venice_International_Film_Festival-1_%28cropped%29.jpg/330px-Aaron_Taylor-Johnson_at_82nd_Venice_International_Film_Festival-1_%28cropped%29.jpg',
-		},
-	},
-	'9': {
-		position: { x: 666, y: 100 },
-		data: {
-			label: 'Damiano David',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Damiano_David_interview_with_Musica_e_TV_2.0_in_2025.png/330px-Damiano_David_interview_with_Musica_e_TV_2.0_in_2025.png',
-		},
-	},
-	'10': {
-		position: { x: 700, y: 200 },
-		data: {
-			label: 'Christian Bale',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Christian_Bale-7837.jpg/330px-Christian_Bale-7837.jpg',
-		},
-	},
-	'11': {
-		position: { x: 733, y: 300 },
-		data: {
-			label: 'Oscar Isaac',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Oscar_Isaac_at_82nd_Venice_International_Film_Festival-1_%28cropped%29.jpg/330px-Oscar_Isaac_at_82nd_Venice_International_Film_Festival-1_%28cropped%29.jpg',
-		},
-	},
-	'12': {
-		position: { x: 166, y: 400 },
-		data: {
-			label: 'Paul Mescal',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Paul_Mescal_at_the_Toronto_International_Film_Festival_in_2025_2_%28cropped_2%29.jpg/330px-Paul_Mescal_at_the_Toronto_International_Film_Festival_in_2025_2_%28cropped_2%29.jpg',
-		},
-	},
-	'13': {
-		position: { x: 100, y: 100 },
-		data: {
-			label: 'Денис Sexy ITI',
-			img: 'https://i0.wp.com/apptractor.ru/wp-content/uploads/2016/11/Denis.jpg',
+			label: 'Кирнан Шипка',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Kiernan_Shipka_Red_Cropped.jpg/330px-Kiernan_Shipka_Red_Cropped.jpg',
 		},
 	},
 };
