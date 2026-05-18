5c8502d63b630b3e103c0fefe4039f974df9b235
konstantin <gktin@yandex.ru>
2026-05-18 16:34:15 +0700

    add position update

--- src/components/BoardItems/components/DndNode/DndNode.tsx
+++ src/components/BoardItems/components/DndNode/DndNode.tsx
@@ -1,5 +1,5 @@
 import type React from 'react';
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import type { Position } from '@/types';
 
 import css from './styles.module.css';
@@ -12,14 +12,36 @@ interface Props extends React.PropsWithChildren {
 
 export const DndNode: React.FC<Props> = ({ children, position, onStop }) => {
 	const nodeRef = useRef<HTMLDivElement>(null);
+	const [localPosition, setLocalPosition] = useState<Position>(position);
+	const isDragging = useRef(false);
 
-	const onDragStop: DraggableEventHandler = (_, { x, y }) => onStop({ x, y });
+	// todo: avoid useEffect, use position as source of truth
+	useEffect(() => {
+		if (!isDragging.current) {
+			setLocalPosition(position);
+		}
+	}, [position.x, position.y]);
+
+	const onDragStart: DraggableEventHandler = () => {
+		isDragging.current = true;
+	};
+
+	const onDrag: DraggableEventHandler = (_, { x, y }) => {
+		setLocalPosition({ x, y });
+	};
+
+	const onDragStop: DraggableEventHandler = (_, { x, y }) => {
+		isDragging.current = false;
+		onStop({ x, y });
+	};
 
 	return (
 		<Draggable
+			onStart={onDragStart}
+			onDrag={onDrag}
 			onStop={onDragStop}
 			nodeRef={nodeRef}
-			defaultPosition={position}
+			position={localPosition}
 			bounds="parent"
 		>
 			<div ref={nodeRef} className={css.dnd}>
